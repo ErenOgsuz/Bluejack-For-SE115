db9ffd68b94572ce0b9061002c343412f9fe50fa
ErenOg <erenogsuz@hotmail.com>
2023-12-23 23:20:50 +0300

    Visual Studio fix & jar file added
    Visual Studio made its own adjustments on the code. Bluejack.jar file created.

--- Bluejack.java
+++ Bluejack.java
@@ -1,113 +1,113 @@
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.nio.file.Paths;
-import java.time.format.DateTimeFormatter;  
+import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.util.InputMismatchException;
 
-public class BlueJack{
-	
+public class BlueJack {
+
 	private static Scanner sc = new Scanner(System.in);
 	private static final int MAX_GAMES = 10;
-    private static final String FILE_PATH = "game_winners.txt";
-	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+	private static final String FILE_PATH = "game_winners.txt";
+	private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 	private static LocalDateTime now = LocalDateTime.now();
 	private static String playerName = new String();
-	
-	public static String getPlayerName(){
+
+	public static String getPlayerName() {
 		return playerName;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		String[] scores = loadScores();
-		
+
 		System.out.println("");
 		System.out.println("Play a new game(1) or Show previous games(2)");
-		int a=0;
+		int a = 0;
 		while (true) {
-            try {
-                System.out.print("Enter your choice: ");
-                if (sc.hasNextInt()) {
+			try {
+				System.out.print("Enter your choice: ");
+				if (sc.hasNextInt()) {
 					a = sc.nextInt();
-					if(a>0 && a<= 2){
+					if (a > 0 && a <= 2) {
 						break;
-					}else{
+					} else {
 						System.out.println("Invalid input. Please try again.");
 					}
-                } else {
-                    System.out.println("Invalid input. Please try again.");
-                    sc.next();
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please try again.");
-                sc.next();
-            }
-        }
-		
+				} else {
+					System.out.println("Invalid input. Please try again.");
+					sc.next();
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input. Please try again.");
+				sc.next();
+			}
+		}
+
 		System.out.println("");
-		
-		if(a == 1){
+
+		if (a == 1) {
 			System.out.println("What is the players name?");
 			System.out.print("Enter your name: ");
 			playerName = sc.next();
 			System.out.println("");
 			Decks.ConstructDecks();
 			Game.GameRun();
-			//String score = new String("Player:" + Game.GetPlayerWin() + " - " + "Computer:" + Game.GetCompWin() + ", " dtf.format(now));
+			// String score = new String("Player:" + Game.GetPlayerWin() + " - " +
+			// "Computer:" + Game.GetCompWin() + ", " dtf.format(now));
 			saveScore(scores);
-		}else if(a == 2){
+		} else if (a == 2) {
 			printScores(scores);
 		}
-		
-		//cd OneDrive\Masaüstü\BluejackProject\Bluejack-For-SE115
-		
-		
+
+		// cd OneDrive\Masaüstü\BluejackProject\Bluejack-For-SE115
+
 	}
-	
+
 	private static String[] loadScores() {
-        String[] scores = new String[MAX_GAMES];
-        try (Scanner reader = new Scanner(Paths.get(FILE_PATH))) {
-            int i = 0;
-            while (reader.hasNextLine() && i < MAX_GAMES) {
-                scores[i++] = reader.nextLine();
-            }
-        }catch (IOException e) {
-            System.out.println("No previous winners found. Starting fresh.");
-        }
-        return scores;
-    }
-	
+		String[] scores = new String[MAX_GAMES];
+		try (Scanner reader = new Scanner(Paths.get(FILE_PATH))) {
+			int i = 0;
+			while (reader.hasNextLine() && i < MAX_GAMES) {
+				scores[i++] = reader.nextLine();
+			}
+		} catch (IOException e) {
+			System.out.println("No previous winners found. Starting fresh.");
+		}
+		return scores;
+	}
+
 	private static void printScores(String[] scores) {
-        System.out.println("Last " + Math.min(MAX_GAMES, countScores(scores)) + " game winners:");
-        for (String score : scores) {
-            if (score != null) {
-                System.out.println(score);
-            }
-        }
-    }
-	
+		System.out.println("Last " + Math.min(MAX_GAMES, countScores(scores)) + " game winners:");
+		for (String score : scores) {
+			if (score != null) {
+				System.out.println(score);
+			}
+		}
+	}
+
 	private static int countScores(String[] scores) {
-        int count = 0;
-        for (String score : scores) {
-            if (score != null) {
-                count++;
-            }
-        }
-        return count;
-    }
-	
+		int count = 0;
+		for (String score : scores) {
+			if (score != null) {
+				count++;
+			}
+		}
+		return count;
+	}
+
 	private static void saveScore(String[] scores) {
 		System.out.println("Saving game...");
 		try {
 			String[] newScores = new String[MAX_GAMES];
-		
-			for(int i = 0; i < scores.length-1; i++){
+
+			for (int i = 0; i < scores.length - 1; i++) {
 				newScores[i] = scores[i + 1];
 			}
 
-			newScores[Math.min(MAX_GAMES - 1, scores.length - 1)] = playerName + ":" + Game.GetPlayerWin() + " - " + "Computer:" + Game.GetCompWin() + ", " + dtf.format(now);
+			newScores[Math.min(MAX_GAMES - 1, scores.length - 1)] = playerName + ":" + Game.GetPlayerWin() + " - "
+					+ "Computer:" + Game.GetCompWin() + ", " + dtf.format(now);
 
 			FileWriter writer = new FileWriter(FILE_PATH);
 
@@ -125,6 +125,3 @@ public class BlueJack{
 	}
 
 }
-
-
-    
--- Game.java
+++ Game.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
-public class Game{
-	
+
+public class Game {
+
 	private static Scanner sc = new Scanner(System.in);
-	
+
 	private static boolean playerStand = false;
 	private static boolean compStand = false;
 	private static boolean playerBust = false;
@@ -12,62 +13,62 @@ public class Game{
 	private static int playerWinRound = 0;
 	private static int compWinRound = 0;
 	private static int gameSet = 0;
-	private static int pBIndex = 0; //player board index
-	private static int cBIndex = 0; //computer board index
+	private static int pBIndex = 0; // player board index
+	private static int cBIndex = 0; // computer board index
 	private static int playerSum = 0;
 	private static int compSum = 0;
-	
-	public static int GetPlayerWin(){
+
+	public static int GetPlayerWin() {
 		return playerWinRound;
 	}
-	
-	public static int GetCompWin(){
+
+	public static int GetCompWin() {
 		return compWinRound;
 	}
-	
-	public static void SetPlayerWin(){
+
+	public static void SetPlayerWin() {
 		playerWinRound++;
 	}
-	
-	public static void SetCompWin(){
+
+	public static void SetCompWin() {
 		compWinRound++;
 	}
-	
-	public static void GameRun(){
-		
-		do{
-			System.out.println("-------------New Set Begins------------");
+
+	public static void GameRun() {
+
+		do {
+			System.out.println("-----------------------New Set Begins----------------------");
 			System.out.println("");
-			do{
-				if(!playerStand){
+			do {
+				if (!playerStand && !compBust) {
 					PlayerTurn();
 				}
-				if(!compStand){
+				if (!compStand && !playerBust) {
 					CompTurn();
 				}
-			}while((!playerBust) && (!compBust) && !(compStand && playerStand));
-			
-			if(compBust){
+			} while ((!playerBust) && (!compBust) && !(compStand && playerStand));
+
+			if (compBust) {
 				SetPlayerWin();
 				System.out.println("Player won the set");
-			}else if(playerBust){
+			} else if (playerBust) {
 				SetCompWin();
 				System.out.println("Computer won the set");
-			}else if(playerStand && compStand){
-				if(playerSum > compSum){
+			} else if (playerStand && compStand) {
+				if (playerSum > compSum) {
 					SetPlayerWin();
 					System.out.println("Player won the set");
-				}else if(playerSum==compSum){
+				} else if (playerSum == compSum) {
 					System.out.println("Set is tied");
-				}else{
+				} else {
 					SetCompWin();
 					System.out.println("Computer won the set");
 				}
-			}else{
+			} else {
 				SetCompWin();
 				System.out.println("Computer won the set");
 			}
-			
+
 			compBust = false;
 			playerBust = false;
 			playerStand = false;
@@ -79,22 +80,23 @@ public class Game{
 			gameSet++;
 			System.out.println("Set is over");
 			System.out.println("");
-			for(int i= 0; i< Decks.GetPlayerBoard().length; i++){
-				Decks.GetPlayerBoard()[i] = new Card(0,null,null);
+			for (int i = 0; i < Decks.GetPlayerBoard().length; i++) {
+				Decks.GetPlayerBoard()[i] = new Card(0, null, null);
 			}
-			for(int i= 0; i< Decks.GetCompBoard().length; i++){
-				Decks.GetCompBoard()[i] = new Card(0,null,null);
+			for (int i = 0; i < Decks.GetCompBoard().length; i++) {
+				Decks.GetCompBoard()[i] = new Card(0, null, null);
 			}
-		}while(gameSet < 3);
+		} while (gameSet < 3);
 		System.out.println("Game Over");
-		System.out.println("Score: " + BlueJack.getPlayerName() + " " + Game.GetPlayerWin() + " - "+ "Computer " + Game.GetCompWin());
+		System.out.println("Score: " + BlueJack.getPlayerName() + " " + Game.GetPlayerWin() + " - " + "Computer "
+				+ Game.GetCompWin());
 		System.out.println("");
-		
+
 	}
-	
-	//ask a card
-	public static Card AskCard(){
-		if(Decks.GetDeckTop() == Decks.GetDeckBottom()){
+
+	// ask a card
+	public static Card AskCard() {
+		if (Decks.GetDeckTop() == Decks.GetDeckBottom()) {
 			playerStand = true;
 			compStand = true;
 			gameSet = 3;
@@ -103,274 +105,279 @@ public class Game{
 		Decks.SetDeckTop(-1);
 		return asked;
 	}
-	
-	public static int Choose(int max) {
-        int a = 0;
 
-        while (true) {
-            try {
-                System.out.print("Enter your choice: ");
-                if (sc.hasNextInt()) {
+	public static int Choose(int max) {
+		int a = 0;
+
+		while (true) {
+			try {
+				System.out.print("Enter your choice: ");
+				if (sc.hasNextInt()) {
 					a = sc.nextInt();
-					if(a>0 && a<= max){
+					if (a > 0 && a <= max) {
 						break;
-					}else{
+					} else {
 						System.out.println("Invalid input. Please try again.");
 					}
-                } else {
-                    System.out.println("Invalid input. Please try again.");
-                    sc.next();
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid input. Please try again.");
-                sc.next();
-            }catch (NoSuchElementException e) {
-                System.out.println("Invalid input. Please try again.");
-                sc.next();
-            }catch (Exception ex) {
-                System.out.println("Invalid input. Please try again.");
-                sc.next();
-            }
-        }
+				} else {
+					System.out.println("Invalid input. Please try again.");
+					sc.next();
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input. Please try again.");
+				sc.next();
+			} catch (NoSuchElementException e) {
+				System.out.println("Invalid input. Please try again.");
+				sc.next();
+			} catch (Exception ex) {
+				System.out.println("Invalid input. Please try again.");
+				sc.next();
+			}
+		}
 		System.out.println("");
-        return a;
-    }
-	
-	public static void compCheck(){
+		return a;
+	}
+
+	public static void compCheck() {
 		compSum = 0;
-		for(Card p: Decks.GetCompBoard()){
+		for (Card p : Decks.GetCompBoard()) {
 			compSum = compSum + p.getNumber();
 		}
-		
-		if(compSum > 20){
+
+		if (compSum > 20) {
 			compBust = true;
 			System.out.println("Computer is bust!");
 			System.out.println("");
-			//Start a new game
-		}else if(compSum == 20){
+			// Start a new game
+		} else if (compSum == 20) {
 			compStand = true;
 			int checkedCard = 0;
 			int colorBlue = 0;
-			for(Card p: Decks.GetCompBoard()){
-				if(p.getColor() != null){
+			for (Card p : Decks.GetCompBoard()) {
+				if (p.getColor() != null) {
 					checkedCard++;
-					if(p.getColor().equals("B")){
+					if (p.getColor().equals("B")) {
 						colorBlue++;
 					}
 				}
 			}
-			if(checkedCard == colorBlue){
+			if (checkedCard == colorBlue) {
 				playerBust = true;
 				playerWinRound = 0;
 				compWinRound = 2;
 				gameSet = 3;
 			}
-		}else if(cBIndex == 9 && compSum <= 20){
+		} else if (cBIndex == 9 && compSum <= 20) {
 			playerBust = true;
 		}
 	}
-	
-	public static void playerCheck(){
+
+	public static void playerCheck() {
 		playerSum = 0;
-		for(Card p: Decks.GetPlayerBoard()){
+		for (Card p : Decks.GetPlayerBoard()) {
 			playerSum = playerSum + p.getNumber();
 		}
-		
-		if(playerSum > 20){
+
+		if (playerSum > 20) {
 			playerBust = true;
 			System.out.println("You are bust!");
 			System.out.println("");
-			//Start a new game
-		}else if(playerSum == 20){
+			// Start a new game
+		} else if (playerSum == 20) {
 			int checkedCard = 0;
 			int colorBlue = 0;
-			for(Card p: Decks.GetPlayerBoard()){
-				if(p.getColor() != null){
+			for (Card p : Decks.GetPlayerBoard()) {
+				if (p.getColor() != null) {
 					checkedCard++;
-					if(p.getColor().equals("B")){
+					if (p.getColor().equals("B")) {
 						colorBlue++;
 					}
 				}
 			}
-			if(checkedCard == colorBlue){
+			if (checkedCard == colorBlue) {
 				compBust = true;
 				playerWinRound = 2;
 				compWinRound = 0;
 				gameSet = 3;
 			}
 			playerStand = true;
-		}else if(pBIndex == 9 && playerSum <= 20){
+		} else if (pBIndex == 9 && playerSum <= 20) {
 			compBust = true;
 		}
 	}
-	
-	//player chooses (end turn, stand, play a card from hand)
-	public static void PlayerTurn(){
+
+	// player chooses (end turn, stand, play a card from hand)
+	public static void PlayerTurn() {
 		System.out.println("Player's turn:");
 		System.out.println("");
 		int choose = 0;
-		
+
 		Board.CreateBoard();
-		
+
 		System.out.println("What do you want to do:");
 		System.out.println("1: Want A Card, 2: Stand, 3: Play A Card");
-		
+
 		choose = Choose(3);
-			
-			switch(choose){
-				case 1:
-					//Want a card from dealer
-					Decks.GetPlayerBoard()[pBIndex] = AskCard();
+
+		switch (choose) {
+			case 1:
+				// Want a card from dealer
+				Decks.GetPlayerBoard()[pBIndex] = AskCard();
+				pBIndex++;
+				Board.CreateBoard();
+				playerCheck();
+				break;
+			case 2:
+				// stand
+				playerCheck();
+				playerStand = true;
+				break;
+			case 3:
+				// play a card from hand
+				int cardsInHand = 4;
+				int cardsNotInHand = 0;
+				System.out.println("Which card you want to choose?");
+				for (Card p : Decks.GetPlayerHand()) {
+					if (p.getNumber() == 0 && p.getSpecial() == null) {
+						cardsNotInHand++;
+					}
+				}
+				for (int i = 0; i < (cardsInHand - cardsNotInHand); i++) {
+					System.out.print((i + 1) + ")");
+					Decks.GetPlayerHand()[i].printDeck();
+					System.out.print(" ");
+				}
+				System.out.println("");
+				choose = Choose((cardsInHand - cardsNotInHand));
+
+				if (Decks.GetPlayerHand()[choose - 1].getNumber() == 0
+						&& Decks.GetPlayerHand()[choose - 1].getSpecial().equals("x2")) {
+					if (pBIndex > 0) {
+						Decks.GetPlayerBoard()[pBIndex - 1]
+								.setNumber(Decks.GetPlayerBoard()[pBIndex - 1].getNumber() * 2);
+					}
+					Decks.GetPlayerBoard()[pBIndex] = Decks.GetPlayerHand()[choose - 1];
 					pBIndex++;
-					Board.CreateBoard();
-					playerCheck();
-					break;
-				case 2:
-					//stand
-					playerCheck();
-					playerStand = true;
-					break;
-				case 3:
-					//play a card from hand
-					int cardsInHand = 4;
-					int cardsNotInHand = 0;
-					System.out.println("Which card you want to choose?");
-					for(Card p: Decks.GetPlayerHand()){
-							if(p.getNumber() == 0 && p.getSpecial() == null){
-								cardsNotInHand++;
-							}
+				} else if (Decks.GetPlayerHand()[choose - 1].getNumber() == 0
+						&& Decks.GetPlayerHand()[choose - 1].getSpecial().equals("+/-")) {
+					if (pBIndex > 0) {
+						Decks.GetPlayerBoard()[pBIndex - 1]
+								.setNumber(Decks.GetPlayerBoard()[pBIndex - 1].getNumber() * -1);
 					}
-					for(int i = 0; i < (cardsInHand - cardsNotInHand); i++){
-						System.out.print((i +1) + ")"); 
-						Decks.GetPlayerHand()[i].printDeck();
-						System.out.print(" ");
-					}
-					System.out.println("");
-					choose = Choose((cardsInHand - cardsNotInHand));
-					
-					if(Decks.GetPlayerHand()[choose-1].getNumber() == 0 && Decks.GetPlayerHand()[choose-1].getSpecial().equals("x2")){
-						if(pBIndex > 0){
-							Decks.GetPlayerBoard()[pBIndex-1].setNumber(Decks.GetPlayerBoard()[pBIndex-1].getNumber() * 2);
-						}
-						Decks.GetPlayerBoard()[pBIndex] = Decks.GetPlayerHand()[choose-1];
-						pBIndex++;
-					}else if(Decks.GetPlayerHand()[choose-1].getNumber() == 0 && Decks.GetPlayerHand()[choose-1].getSpecial().equals("+/-")){
-						if(pBIndex > 0){
-							Decks.GetPlayerBoard()[pBIndex-1].setNumber(Decks.GetPlayerBoard()[pBIndex-1].getNumber() * -1);
-						}
-						Decks.GetPlayerBoard()[pBIndex] = Decks.GetPlayerHand()[choose-1];
-						pBIndex++;
-					}else{
-						Decks.GetPlayerBoard()[pBIndex] = Decks.GetPlayerHand()[choose-1];
-						pBIndex++;
-					}
-					
-					Decks.UpdatePlayerHand((choose - 1));
-					Board.CreateBoard();
-					
-					playerCheck();
-					break;
-			}
+					Decks.GetPlayerBoard()[pBIndex] = Decks.GetPlayerHand()[choose - 1];
+					pBIndex++;
+				} else {
+					Decks.GetPlayerBoard()[pBIndex] = Decks.GetPlayerHand()[choose - 1];
+					pBIndex++;
+				}
+
+				Decks.UpdatePlayerHand((choose - 1));
+				Board.CreateBoard();
+
+				playerCheck();
+				break;
+		}
 	}
-	
-	public static void CompTurn(){
+
+	public static void CompTurn() {
 		System.out.println("Computer's turn:");
 		System.out.println("");
 		int choose = 2;
 		int chooseCard = 0;
 		compSum = 0;
-		for(Card p: Decks.GetCompBoard()){
+		for (Card p : Decks.GetCompBoard()) {
 			compSum = compSum + p.getNumber();
 		}
-		
-		//An algorithm for computer to choose
-		if(playerStand && (playerSum < compSum)){
+
+		// An algorithm for computer to choose
+		if (playerStand && (playerSum < compSum)) {
 			choose = 2;
-		}else{
-			if(compSum < 15){
+		} else {
+			if (compSum < 15) {
 				choose = 1;
-			}else if((compSum >= 15 && compSum < 20)|| compSum > 20){
-				for(int i= 0; i < Decks.GetCompHand().length; i++){
-					if(Decks.GetCompHand()[i].getNumber() + compSum == 20){
-						//use this card
+			} else if ((compSum >= 15 && compSum < 20) || compSum > 20) {
+				for (int i = 0; i < Decks.GetCompHand().length; i++) {
+					if (Decks.GetCompHand()[i].getNumber() + compSum == 20) {
+						// use this card
 						choose = 3;
 						chooseCard = i;
-					}else{
-						if(Decks.GetCompHand()[i].getNumber() == 0){
-							if(Decks.GetCompHand()[i].getSpecial().equals("x2")){
-								if(compSum + Decks.GetCompBoard()[cBIndex-1].getNumber() == 20){
-									//use this card
+					} else {
+						if (Decks.GetCompHand()[i].getNumber() == 0) {
+							if (Decks.GetCompHand()[i].getSpecial().equals("x2")) {
+								if (compSum + Decks.GetCompBoard()[cBIndex - 1].getNumber() == 20) {
+									// use this card
 									choose = 3;
 									chooseCard = i;
 								}
-							}else if(Decks.GetCompHand()[i].getSpecial().equals("+/-")){
-								if(compSum + (Decks.GetCompBoard()[cBIndex-1].getNumber() * -2) == 20){
-									//use this card
+							} else if (Decks.GetCompHand()[i].getSpecial().equals("+/-")) {
+								if (compSum + (Decks.GetCompBoard()[cBIndex - 1].getNumber() * -2) == 20) {
+									// use this card
 									choose = 3;
 									chooseCard = i;
 								}
-							}else{
+							} else {
 								continue;
 							}
-						}else{
+						} else {
 							continue;
 						}
 					}
 				}
-			}else if(compSum == 20){
+			} else if (compSum == 20) {
 				choose = 2;
-			}else{
+			} else {
 				choose = 2;
 			}
 		}
-			
-			
-		switch(choose){
-				case 1:
-					//Computer asks for a card
-					System.out.println("Computer asks for a card");
-					System.out.println("");
-					Decks.GetCompBoard()[cBIndex] = AskCard();
-					cBIndex++;
-					Board.CreateBoard();
-					compCheck();
-					break;
-				case 2:
-					//stand
-					System.out.println("Computer stands");
-					System.out.println("");
-					Board.CreateBoard();
-					compCheck();
-					compStand = true;
-					break;
-				case 3:
-					//An algorithm for computer to choose again
-					System.out.println("Computer played a card");
-					System.out.println("");
-					
-					if(Decks.GetCompHand()[chooseCard].getNumber() == 0 && Decks.GetCompHand()[chooseCard].getSpecial().equals("x2")){
-						if(cBIndex > 0){
-							Decks.GetCompBoard()[cBIndex-1].setNumber(Decks.GetCompBoard()[cBIndex-1].getNumber() * 2);
-						}
-						Decks.GetCompBoard()[cBIndex] = Decks.GetCompHand()[chooseCard];
-						cBIndex++;
-					}else if(Decks.GetCompHand()[chooseCard].getNumber() == 0 && Decks.GetCompHand()[chooseCard].getSpecial().equals("+/-")){
-						if(cBIndex > 0){
-							Decks.GetCompBoard()[cBIndex-1].setNumber(Decks.GetCompBoard()[cBIndex-1].getNumber() * -1);
-						}
-						Decks.GetCompBoard()[cBIndex] = Decks.GetCompHand()[chooseCard];
-						cBIndex++;
-					}else{
-						Decks.GetCompBoard()[cBIndex] = Decks.GetCompHand()[chooseCard];
-						cBIndex++;
+
+		switch (choose) {
+			case 1:
+				// Computer asks for a card
+				System.out.println("Computer asks for a card");
+				System.out.println("");
+				Decks.GetCompBoard()[cBIndex] = AskCard();
+				cBIndex++;
+				Board.CreateBoard();
+				compCheck();
+				break;
+			case 2:
+				// stand
+				System.out.println("Computer stands");
+				System.out.println("");
+				Board.CreateBoard();
+				compCheck();
+				compStand = true;
+				break;
+			case 3:
+				// An algorithm for computer to choose again
+				System.out.println("Computer played a card");
+				System.out.println("");
+
+				if (Decks.GetCompHand()[chooseCard].getNumber() == 0
+						&& Decks.GetCompHand()[chooseCard].getSpecial().equals("x2")) {
+					if (cBIndex > 0) {
+						Decks.GetCompBoard()[cBIndex - 1].setNumber(Decks.GetCompBoard()[cBIndex - 1].getNumber() * 2);
 					}
-					
-					Decks.GetCompHand()[chooseCard] = new Card(0,"","");
-					Board.CreateBoard();
-					
-					compCheck();
-					break;
+					Decks.GetCompBoard()[cBIndex] = Decks.GetCompHand()[chooseCard];
+					cBIndex++;
+				} else if (Decks.GetCompHand()[chooseCard].getNumber() == 0
+						&& Decks.GetCompHand()[chooseCard].getSpecial().equals("+/-")) {
+					if (cBIndex > 0) {
+						Decks.GetCompBoard()[cBIndex - 1].setNumber(Decks.GetCompBoard()[cBIndex - 1].getNumber() * -1);
+					}
+					Decks.GetCompBoard()[cBIndex] = Decks.GetCompHand()[chooseCard];
+					cBIndex++;
+				} else {
+					Decks.GetCompBoard()[cBIndex] = Decks.GetCompHand()[chooseCard];
+					cBIndex++;
+				}
+
+				Decks.GetCompHand()[chooseCard] = new Card(0, "", "");
+				Board.CreateBoard();
+
+				compCheck();
+				break;
 		}
-		
+
 	}
 }

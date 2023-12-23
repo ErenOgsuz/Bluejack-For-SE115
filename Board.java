20637be4a7559345bbb565161ee8b9acf9091f9c
ErenOg <erenogsuz@hotmail.com>
2023-12-23 21:34:40 +0300

    Improved visuals
    Improved on the visuals of game board. Game boards size now can hold up to 9 cards in board and board automatically sums the cards.

--- Board.java
+++ Board.java
@@ -1,7 +1,7 @@
 public class Board{
 	public static void CreateBoard(){
 		System.out.println("Score: " + BlueJack.getPlayerName() + " " + Game.GetPlayerWin() + " - "+ "Computer " + Game.GetCompWin());
-		System.out.println("---------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.print("Computer Hand    | ");
 		for(Card p: Decks.GetCompHand()){
 			if(p.getNumber() == 0 && p.getSpecial().equals("")){
@@ -11,25 +11,35 @@ public class Board{
 			}
 		}
 		System.out.println("");
-		System.out.println("---------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.print("Computer Board   | " );
 		for(Card p: Decks.GetCompBoard()){
 			p.printDeck();
 		}
+		int compSum = 0;
+		for(Card p: Decks.GetCompBoard()){
+			compSum = compSum + p.getNumber();
+		}
+		System.out.print("= " + compSum);
 		System.out.println("");
-		System.out.println("---------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.print("Player Board     | " );
 		for(Card p: Decks.GetPlayerBoard()){
 			p.printDeck();
 		}
+		int playerSum = 0;
+		for(Card p: Decks.GetPlayerBoard()){
+			playerSum = playerSum + p.getNumber();
+		}
+		System.out.print("= " + playerSum);
 		System.out.println("");
-		System.out.println("---------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.print("Player Hand      | "); 
 		for(Card p: Decks.GetPlayerHand()){
 			p.printDeck();
 		}
 		System.out.println("");
-		System.out.println("---------------------------------------");
+		System.out.println("-----------------------------------------------------------");
 		System.out.println("");
 	}
 }
